2b48317abc10cf972c12f156a9a50672b87c3205
Amund Eggen Svandal <aesvandal@gmail.com>
2018-10-17 17:31:11 +0000

    Refactor king move function

--- pieces/king.py
+++ pieces/king.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 from . import moves, WHITE, BLACK
 from .piece import Piece
@@ -16,13 +16,24 @@ class King(Piece):
         if target not in self.getMoves(current):
             return False, None
 
-        if abs(current[1] - target[1]) == 2:
+        if abs(current[1] - target[1]) != 2:
+            # Regular move
+            piece = board[target]
+            if piece is None:
+                return True, None
+            elif piece.color != self.color:
+                return True, None
+        else:
             # Castling move
             if current[0] != (0 if self.color is WHITE else 7):
                 # Can't castle unless on your side's first rank
                 return False, None
             if self.hasMoved:
                 return False, None
+
+            # Cannot castle from check
+            # b.checkTest(current)
+
             # Searching for rook to castle with using a
             # hard coded position, only works in default game
             if target[1] > current[1]:
@@ -36,8 +47,15 @@ class King(Piece):
             castleCheck = False
             rookFile = None
             while scanFile >= 0 and scanFile < 8:
+                # Cannot castle through check
+                # b.checkTest((current[0], scanFile))
+
+                # Scan for rook or empty space
                 square = board[(current[0], scanFile)]
-                if square is not None:
+                if square is None:
+                    # Empty -> search next square
+                    scanFile += dirFile
+                else:
                     if type(square) is Rook:
                         if square.color is self.color and not square.hasMoved:
                             # Rook and path valid to castle
@@ -46,23 +64,10 @@ class King(Piece):
                     else:
                         # Met non-rook -> invalid move
                         return False, None
-                else:
-                    scanFile += dirFile
-
-            if rookFile is None:
+            else:
                 # Found no rook
                 return False, None
-            else:
-                # Found rook and clear path
-                # Cannot castle from, through, or into check
-                # b.checkTest(current)
-                # b.checkTest((0, current[1] + scandir))
-                # Checktest for square landed on done by board
-                return True, [(current[0], rookFile), (current[0], current[1] + dirFile)]
-        else:
-            piece = board[target]
-            if piece is None:
-                return True, None
-            elif piece.color != self.color:
-                return True, None
+
+            # Found rook and clear path
+            return True, [(current[0], rookFile), (current[0], current[1] + dirFile)]
 
